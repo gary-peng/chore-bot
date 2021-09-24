773626bdba3a018aac0c7be9762f7720ce2e9e51
Gary Peng <49541146+gary-peng@users.noreply.github.com>
2021-09-24 16:47:03 -0400

    Heroku scheduler test

---
+++ scheduler/reminder.js
@@ -0,0 +1,29 @@
+import fetch from "node-fetch";
+import { dummy } from "./dummy.js"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+var text = "Reminder to do your chores:\n"
+dummy.chores.forEach(el => {
+    if (!el.status) {
+        text += el.name + ": " + el.assigned + "\n"
+    }
+});
+
+const requestOptions = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "bot_id": process.env.BOT_ID,
+        "text": text
+    }),
+    redirect: 'follow'
+};
+
+fetch("https://api.groupme.com/v3/bots/post", requestOptions)
+.then(response => response.text())
+.then(result => console.log(result))
+.catch(error => console.log('error', error));
+
+
