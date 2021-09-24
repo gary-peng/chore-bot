c1e698217350ec30e4b5598900f75f71e7567f26
Gary Peng <49541146+gary-peng@users.noreply.github.com>
2021-09-24 16:49:47 -0400

    Index fix

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 
 import router from "./routes/command.js";
-import { reminder } from "./utils/reminder.js"
 
 dotenv.config();
 
@@ -13,5 +12,3 @@ app.use(express.json());
 app.use(router);
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-
-reminder();
